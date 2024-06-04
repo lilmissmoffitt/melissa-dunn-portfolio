328d75d066a0734b2a5691afc389e1f2d16b549c
Melissa Dunn <lilmissmoffitt7@gmail.com>
2024-06-03 22:46:13 -0600

    wip page

--- src/App.js
+++ src/App.js
@@ -5,18 +5,15 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+        Melissa Dunn's Portfolio in Progress...
+
+        Come back later or shoot her an email :)
+        </div>
+        <br></br>
+        <div>
+          melissadunncareers@gmail.com
+        </div>
       </header>
     </div>
   );
