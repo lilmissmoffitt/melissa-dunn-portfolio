533dda5a0461ba1211b7959e853f169f4c9c6ebb
Melissa Dunn <lilmissmoffitt7@gmail.com>
2024-06-21 20:03:56 -0600

    minor improvements

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <div className="header-container">
       <Navbar expand="sm" bg="dark" fixed="top" variant="dark">
-        <Navbar.Brand href="#home" style={{paddingLeft: "20px"}}>Melissa Dunn's Portfolio</Navbar.Brand>
+        <Navbar.Brand href="/" style={{paddingLeft: "20px"}}>Melissa Dunn's Portfolio</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" style={{justifyContent: "right", paddingRight: "20px"}}>
           <Nav className="ml-auto">
